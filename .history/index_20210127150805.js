83ca3a0b289897014d2ed031df35a30d35be7268
GeryZlatev <68344068+GeryZlatev@users.noreply.github.com>
2021-01-27 15:12:27 +0200

    add routes

--- .history/index_20210127150805.js
+++ .history/index_20210127150805.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config/config')[env];
-const routes = require('./routes');
+
 const app = express();
 // const expressConfig = require('./config/express');
 // expressConfig(app);
 require('./config/express')(app);
-app.use(routes);
 
+app.get('/', (req, res) => {
+    res.render('home', { layout: false });
+});
 
 app.listen(config.port, () => console.log(`Server is running on port ${config.port}...`));
--- .history/index_20210127150858.js
+++ .history/index_20210127150858.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config/config')[env];
-const routes = require('./routes');
+
 const app = express();
 // const expressConfig = require('./config/express');
 // expressConfig(app);
 require('./config/express')(app);
-app.use(routes);
+
 
 
 app.listen(config.port, () => console.log(`Server is running on port ${config.port}...`));
